f8223e814ff5356cc1e0f7bf6be33a4fb944874e
Cesar San Jose <cksanjose@gmail.com>
2016-04-21 20:27:34 -0500

    Update product component

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from 'angular2/core';
 import { RouteParams, Router } from 'angular2/router';
-
-import { IProduct } from './product';
 import { ProductService } from './product.service';
 import { StarComponent } from '../shared/star.component';
+import { IProduct } from "./products";
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html',
